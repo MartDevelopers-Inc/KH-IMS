0cdde88cbf50c26f80f6dd4de1c47e6cf4b0c8a0
PerpetualNdanu <53426642+perpetualndanu@users.noreply.github.com>
2020-07-22 19:02:43 +0300

    Update footer1.php

--- KH-IMS-Core/backend/doc/assets/inc/footer1.php
+++ KH-IMS-Core/backend/doc/assets/inc/footer1.php
@@ -1,3 +1,3 @@
 <footer class="footer footer-alt">
-            2020 - <?php echo date ('Y');?> &copy; Hospital Managememt Information System. Proudly Powered  By <a href="https://martmbithi.github.io" target = "_blank" class="text-white-50">MartDevelopers</a> 
+            2020 - <?php echo date ('Y');?> &copy; Kangundo Hospital Managememt Information System. Proudly Powered  By <a href="https://martdev.info/" target = "_blank" class="text-white-50">MartDevelopers</a> 
 </footer>
